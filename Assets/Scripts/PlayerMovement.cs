d317d345d85df45c39fd4db795993133172ed892
azrakzlky-beep <azrakzlky@gmail.com>
2026-04-10 17:55:37 +0200

    player sat ind

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using System.Numerics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
